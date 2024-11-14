9c0e30c469bf38589ded3fafc98d180ae06c00d6
PRONIX-DEV-116 <PRONIX-DEV-116@DESKTOP-L2MOSIU>
2024-11-14 18:17:03 +0530

    find by id

--- EmployeeDetails/src/main/java/com/prnx/controller/EmployeeController.java
+++ EmployeeDetails/src/main/java/com/prnx/controller/EmployeeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prnx.entity.Employee;
@@ -33,9 +32,9 @@ public class EmployeeController {
 	}
 	
 	@GetMapping("/findEmployee/{id}")
-	public List<Employee> allEmployees(@PathVariable Long id )
+	public Employee allEmployees(@PathVariable Long id )
 	{
-		return service.getEmployees();
+		return service.findEmployee(id);
 	}
 	
 }
