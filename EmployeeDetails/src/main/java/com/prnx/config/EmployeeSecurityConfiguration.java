32814cd461ff85a8b4f2736fb5ada05582608fe1
PRONIX-DEV-116 <PRONIX-DEV-116@DESKTOP-L2MOSIU>
2024-11-25 19:47:33 +0530

    security

--- EmployeeDetails/src/main/java/com/prnx/config/EmployeeSecurityConfiguration.java
+++ EmployeeDetails/src/main/java/com/prnx/config/EmployeeSecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.prnx.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class EmployeeSecurityConfiguration {
@@ -13,5 +17,19 @@ public class EmployeeSecurityConfiguration {
 		UserDetails raj = User.builder().username("Raj").password("{noop}123").roles("ADMIN").build();
 		UserDetails deepak = User.builder().username("Deepak").password("{noop}123").roles("EMPLOYEE").build();
 		return new InMemoryUserDetailsManager(raj, deepak);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+		
+		http.authorizeHttpRequests(configurer -> 
+			configurer.requestMatchers(HttpMethod.GET, "/allEmployees").hasRole("EMPLOYEE")
+				);
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		http.csrf(csrf -> csrf.disable());
+		return http.build();
 	}
 }
