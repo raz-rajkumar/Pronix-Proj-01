d2cb4848bd45fa8897b6c049bf3c1fe299b434fd
raz-rajkumar <cscrajkumar12@gmail.com>
2024-12-06 00:50:55 +0530

    .

--- EmployeeDetails/src/main/java/com/prnx/controller/EmployeeController.java
+++ EmployeeDetails/src/main/java/com/prnx/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prnx.entity.Employee;
-import com.prnx.exceptionHandling.EmployeeErrorResponse;
 import com.prnx.exceptionHandling.EmployeeNotFoundException;
 import com.prnx.service.EmployeeService;
 
